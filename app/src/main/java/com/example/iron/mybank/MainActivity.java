e065418f1a6f1842bc39a28883505ade40744001
Paul Smith <Paul@bulltwist.com>
2015-03-29 08:08:38 -0400

    Apparently I lack some particular perversion which today's employer is seeking-John Kennedy Toole

--- app/src/main/java/com/example/iron/mybank/MainActivity.java
+++ app/src/main/java/com/example/iron/mybank/MainActivity.java
@@ -4,14 +4,25 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    EditText amountInput;
+    Button withdrawButton;
+    Button depositButton;
+    TextView amountDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        amountDisplay = (TextView) findViewById(R.id.balance_display);
+        amountDisplay.setText("Hello World");
     }
 
 
