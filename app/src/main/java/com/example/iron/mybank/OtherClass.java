3be943eb132f15adc657e947f1617aec319f8e43
Paul Smith <Paul@BullTwist.com>
2015-04-28 16:03:34 -0400

    8-9 Protected

--- app/src/main/java/com/example/iron/mybank/OtherClass.java
+++ app/src/main/java/com/example/iron/mybank/OtherClass.java
@@ -6,4 +6,7 @@ import com.example.iron.accounts.BankAccount;
  * Created by neon on 4/23/15.
  */
 public class OtherClass extends BankAccount{
+    OtherClass(){
+        numberOfWithdrawals();
+    }
 }
